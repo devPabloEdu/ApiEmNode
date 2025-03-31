502c34cb8c6e8c068c3af177e658b2bc8480def7
Pablo <vilelapege1113@gmail.com>
2025-03-31 19:17:15 -0300

    Feat: Try, Catch

--- server.js
+++ server.js
@@ -10,9 +10,15 @@ const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port); //define a key port com o value port, que pode ser referenciado depois dentro do código
 
-const server = http.createServer(app);// Cria o servidor HTTP e passa a instância do Express como argumento
+try {
+    const server = http.createServer(app);// Cria o servidor HTTP e passa a instância do Express como argumento
+    server.listen(port); //utiliza a instancia server e define que o metodo listen recebe como parametro a constante Port definida anteriormente
+        console.log(`o servidor está rodando na porta ${port}`);
+} catch (error) {
+    console.error(`Erro ao Iniciar o Servidor: ${error.message}`);
+    process.exit(1)
+}
 const router = express.Router(); //cria um roteador separado dentro do Express. Em vez de adicionar as rotas diretamente no app, podemos modularizar as rotas em um roteador e depois "conectá-lo" ao app.
-server.listen(port); //utiliza a instancia server e define que o metodo listen recebe como parametro a constante Port definida anteriormente
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -21,8 +27,6 @@ const route = router.get('/', (req, res, next) => {
     });
 });
 app.use('/', route);
-
-console.log(`o servidor est[a rodando na porta ${port}`);
 
 //serve para garantir que a porta usada pelo servidor seja válida e compatível
 function normalizePort(val) {
