4c92ed8eae48bc031e8818a98de1e3c8d5009fa8
Pablo <vilelapege1113@gmail.com>
2025-03-31 21:27:24 -0300

    Post

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const http = require('http');
 const app = express();
 const router = express.Router(); //cria um roteador separado dentro do Express. Em vez de adicionar as rotas diretamente no app, podemos modularizar as rotas em um roteador e depois "conectá-lo" ao app.
 
+app.use(express.json());  // Middleware para analisar JSON no corpo das requisições
+
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node Store Api",
@@ -12,5 +14,11 @@ const route = router.get('/', (req, res, next) => {
     });
 });
 app.use('/', route);
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+app.use('/Products', create);
+
 
 module.exports = app;
