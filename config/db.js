d864408c6fc360faa636a459f48d728ee257b86e
Pablo <vilelapege1113@gmail.com>
2025-04-02 01:09:33 -0300

    Identacao

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 'use strict';
 require('dotenv').config();
 
-async function connect(){
-    if(global.connection)
+async function connect() {
+    if (global.connection)
         return global.connection.connect();
 
     const { Pool } = require('pg');
-    const pool = new Pool ({
+    const pool = new Pool({
         connectionString: process.env.CONNECTION_STRING
     });
 
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('port', port); //define a key port com o value port, que pode ser refere
 try {
     const server = http.createServer(app);// Cria o servidor HTTP e passa a instância do Express como argumento
     server.listen(port); //utiliza a instancia server e define que o metodo listen recebe como parametro a constante Port definida anteriormente
-        console.log(`o servidor está rodando na porta ${port}`);
+    console.log(`o servidor está rodando na porta ${port}`);
 } catch (error) {
     console.error(`Erro ao Iniciar o Servidor: ${error.message}`);
     process.exit(1)
@@ -25,7 +25,7 @@ function normalizePort(val) {
     if (isNaN(port)) {
         return val;
     }
-    if (port>=0) {
+    if (port >= 0) {
         return port;
     }
 
